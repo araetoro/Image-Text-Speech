9dc1c04390f8109d82b364506473b6010ffbe0c7
araetoro <41526924+araetoro@users.noreply.github.com>
2021-02-16 12:50:16 -0700

    Add files via upload

---
+++ imageToText.py
@@ -0,0 +1,17 @@
+# importing the libraries
+import pytesseract
+from PIL import Image
+import pyttsx3
+from googletrans import Translator
+
+# open image
+img = Image.open(r'C:\Users\amber.toro\PycharmProjects\imageToText\images\test.png')
+
+# prints image format
+print(img)
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+result = pytesseract.image_to_string(img)
+
+print(result)
